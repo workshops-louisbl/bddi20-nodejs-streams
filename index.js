864c1e5d715ffe135fc8a77190309a5a1b110b2c
louisbl <louis@beltramo.me>
2021-01-19 10:05:51 +0100

    Streaming HTTP server

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello World")
+const fs = require("fs")
+const http = require("http")
+
+const server = http.createServer()
+
+server.on("request", (req, res) => {
+  res.writeHead(200)
+  const fileStream = fs.createReadStream("./big.txt")
+  fileStream.pipe(res)
+})
+
+server.listen(3000)
